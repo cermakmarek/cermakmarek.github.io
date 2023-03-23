b036400e5b1a1dbfb693c818e1cef5fb2b14559a
Marek Čermák <cermmarek@gmail.com>
2023-03-23 16:37:51 +0100

    app

---
+++ sfi/app/js/utils.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var newline = "\n";
+
+    new Blob([newline], { endings: 'native' }).text().then(value => {
+        newline = value;
+    });
+
+    getNewline = function() {
+        return newline;
+    }
+})();
+
+function createArray() {
+    return [];
+}
+
+function appendBytes(array, data) {
+    array.push(new Uint8Array(data));
+}
+
+function finalizeBlob(array, mediaType) {
+    var blob = new Blob(array, { type: mediaType });
+    return URL.createObjectURL(blob);
+}
